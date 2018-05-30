061598c55b2220c71ffae9c5ea931b3e35761076
Thorn Wu <thornwu@163.com>
2018-05-30 22:50:24 +0800

    add-distance&createtime-into-positionlist-item

--- app/src/main/java/com/thorn/wego/Adapter/PositionListItemAdapter.java
+++ app/src/main/java/com/thorn/wego/Adapter/PositionListItemAdapter.java
@@ -11,6 +11,7 @@ import com.thorn.wego.Element.PositionListItem;
 import com.thorn.wego.Location.PositionList.Presenter.IPositionListPresenter;
 import com.thorn.wego.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,12 +49,28 @@ public class PositionListItemAdapter extends BaseAdapter{
         TextView positionName = (TextView) view.findViewById(R.id.position_list_name);
         TextView positionAddress = (TextView) view.findViewById(R.id.position_list_address);
         TextView positionCategory = (TextView) view.findViewById(R.id.position_list_category);
+        TextView positionAddition = (TextView) view.findViewById(R.id.position_list_addition);
 
         positionId.setText(String.valueOf(positionItem.getVenueid()));
         positionName.setText(positionItem.getVenuename());
         positionAddress.setText(positionItem.getAddress());
         positionCategory.setText(positionItem.getCategory());
 
+        // 判断有没有 distance 字段
+        if(positionItem.getDistance()!=0){
+            // 判断 distance 字符是否异常
+            if(positionItem.getDistance()>0){
+                Double distance = (double) Math.round(positionItem.getDistance()*100)/100;
+                String text = distance.toString() + "KM";
+                positionAddition.setText(text);
+            }
+        }else{
+            SimpleDateFormat format =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Long time= Math.round(positionItem.getCreatetime()*1000);
+            String d = format.format(time);
+            System.out.println("Format To String(Date):"+d);
+            positionAddition.setText(d);
+        }
 
         return view;
     }
